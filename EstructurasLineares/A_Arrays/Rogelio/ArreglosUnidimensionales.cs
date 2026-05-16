15be2a1585ce806a7e4e5288aecb4ed97ded1fff
Rogelio Reyna <rogelioreynar@outlook.com>
2026-05-16 11:04:45 -0600

    Archivo Rogelio

---
+++ EstructurasLineares/A_Arrays/Rogelio/ArreglosUnidimensionales.cs
@@ -0,0 +1,37 @@
+/* Arreglos Unidimensionales
+Estructura que guarda distintos elementos del mismo tipo en la misma línea/lista
+Ejemplo: 
+int [] numeros = {13,3,22};
+
+Métodos
+
+Búsqueda: Para buscar un elemento
+string[] frutas = { "Ciruela", "Platano", "Uva" };
+Console.WriteLine(frutas[1]);
+
+Actualización: Para cambiar un elemento
+string[] frutas = { "Ciruela", "Platano", "Uva" };
+frutas[1] = "Sandía";
+Console.WriteLine(frutas[1]);
+
+Eliminación: Para borrar un elemento
+
+
+Ventajas:
+Permiten guardar muchos datos
+Ayudan a organizar información
+Es rápido
+
+
+Desventajas:
+Deben ser el mismo tipo
+En algunos lenguajes, es limitado 
+Si se elimina algún elemento, altera el posicionamiento y causar desorden
+
+
+
+Mini ejercicio:
+let nombres = ["Fiona", "Fredy", "Cosmo"];
+console.log(nombres);
+nombres[2] = "Fredo";
+console.log(nombres);
