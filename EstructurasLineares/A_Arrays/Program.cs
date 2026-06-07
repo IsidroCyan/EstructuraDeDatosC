4a992618a013af61a9f776f416c55eb64d32c83b
Rogelio Reyna <rogelioreynar@outlook.com>
2026-06-07 11:31:12 -0600

    Implementación de Clase para una cuenta de ahorro usando un arreglo

--- EstructurasLineares/A_Arrays/Program.cs
+++ EstructurasLineares/A_Arrays/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿CuentaBancaria CuentaAhorro = new CuentaBancaria (5);
+    //*llamar el metodo, colocando el objeto y después el metodo
+ //*adentro del parentesis, cuando llamamos la funcion pasamos a un argumento
+
+
+CuentaAhorro.AgregarMonto(529);
+CuentaAhorro.AgregarMonto(800);
+CuentaAhorro.AgregarMonto(498);
+CuentaAhorro.AgregarMonto(901);
+CuentaAhorro.AgregarMonto(1000);
+//[10,50,100,50,30]
+//CuentaAhorro.EliminarMonto(2)
+//[10,50,0,50,30]
+CuentaAhorro.Average();
+CuentaAhorro.EliminarMonto(2);
+CuentaAhorro.mostrardatos();
